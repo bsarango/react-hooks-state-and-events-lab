98941da82f7c345166f3e25c2768dc5eba0cefbd
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-08-03 13:21:47 -0400

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const[itemAdded,setItem]=useState(false)
+  const itemClass = itemAdded ? "in-cart":"";
+
+  const buttonMessage = itemAdded ? "Remove From Cart":"Add to Cart"
+  function handleSettingItem(){
+    setItem((itemAdded)=>!itemAdded)
+  }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleSettingItem} className="add">{buttonMessage}</button>
     </li>
   );
 }
